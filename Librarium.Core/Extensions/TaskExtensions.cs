bc851f56432a766e58ff6332cc667da2634a2937
David Palmer <davidivpalmer@gmail.com>
2023-02-26 01:26:12 -0600

    :sparkles: Added TaskExtensions.

---
+++ Librarium.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Librarium.Core.Extensions
+{
+    /// <summary>
+    /// Extensions relating to tasks and async programming.
+    /// </summary>
+    [PublicAPI]
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Invokes a delegate, but only if there are any subscribers to the event.
+        /// </summary>
+        /// <typeparam name="T">The type of the delegate.</typeparam>
+        /// <param name="invocation">The method to perform. Ideally, this should be Invoke.</param>
+        /// <returns>An awaitable task.</returns>
+        public static Task InvokeIfAny<T>([CanBeNull] this T? del, [NotNull] Func<T, Task> invocation) where T : Delegate =>
+            del is null ? Task.CompletedTask : invocation(del!);
+    }
+}
